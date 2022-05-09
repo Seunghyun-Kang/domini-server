3e63f3b3b5bd37f9a2880248b4c1f0383573b42b
Seunghyun-Kang <kiseyno@gmail.com>
2022-05-09 22:26:32 +0900

    change settings.py

--- domini/domini/settings.py
+++ domini/domini/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'main'
+    # 'main.apps.MainConfig',
+    'main',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

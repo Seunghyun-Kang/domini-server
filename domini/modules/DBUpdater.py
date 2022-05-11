3e1ef8fa399472135eb7894a3c3a54e1f8e0ca86
Seunghyun-Kang <kiseyno@gmail.com>
2022-05-11 16:20:48 +0900

    Fix issue

--- domini/modules/DBUpdater.py
+++ domini/modules/DBUpdater.py
@@ -54,6 +54,6 @@ for index, item in enumerate(data.itertuples()):
         end_date = None
 
 with conn.cursor() as curs:
-    sql = f"REPLACE INTO airline_events VALUES ('{index}', '{item.항공사}', '{item.카드사}', '{item.통신사}', '{item.페이앱}', '{item.최소비용}', '{item.할인금액}' , '{item.주중/주말}', '{item.편도/왕복}', '{event}', '{item.할인방식}', '{item.중복가능여부}', '{item.특이사항}', '{start_date}', '{end_date}')"
+    sql = f"REPLACE INTO airline_events VALUES ('{index}', '{item['항공사']}', '{item['카드사']}', '{item['통신사']}', '{item['페이앱']}', '{item['최소비용']}', '{item['할인금액']}' , '{item['주중/주말']}', '{item['편도/왕복']}', '{event}', '{item['할인방식']}', '{item['중복가능여부']}', '{item['특이사항']}', '{start_date}', '{end_date}')"
     curs.execute(sql)
     conn.commit()
